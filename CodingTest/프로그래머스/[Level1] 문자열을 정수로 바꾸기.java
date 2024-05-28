943fc93929ac7b1796d30a492ed412850574c44c
희은 <datakubit2@gmail.com>
2024-05-28 17:24:43 +0900

    [Level1] 문자열을 정수로 바꾸기

---
+++ CodingTest/프로그래머스/[Level1] 문자열을 정수로 바꾸기.java
@@ -0,0 +1,15 @@
+/*
+ * 문제링크: https://school.programmers.co.kr/learn/courses/30/lessons/12925?language=java
+ * 레벨: level1
+ * 알고리즘: X
+ */
+
+package 프로그래머스;
+class StringToInt {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
